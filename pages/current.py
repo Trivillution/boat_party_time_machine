527df7cfc8e116430b64869aaa298ef8e75d85ed
Yuanjie Chen <yuanjiechen@Yuanjies-MacBook-Air.local>
2022-07-17 23:25:51 +0100

    test image and emoji in page name

--- pages/current.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title('CCS Alumni Boat Party 2022')
-
-st.header('二维码 “嘿，朋友”')
--- run_app.py
+++ run_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from PIL import Image
 
 st.title('CCS Alumni Boat Party 2022')
 
-st.header('二维码 “过去”')
+image = Image.open('/data/logo.PNG')
+
+st.image(image)
