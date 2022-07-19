c0fc32560321ab5dd44fda61f63b1d0be7173e50
Yuanjie Chen <yuanjiechen@Yuanjies-MacBook-Air.local>
2022-07-20 00:22:35 +0100

    try aws access on streamlit server

--- Boat_Party_Time_Machine⏱.py
+++ Boat_Party_Time_Machine⏱.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
+import s3fs
+import os
 
 image = Image.open('./data/logo.PNG')
 st.image(image, width=200)
@@ -11,7 +13,16 @@ st.subheader('About US')
 st.subheader('technical support contact')
 
 st.subheader('留言板')
-messages_df = pd.read_csv('./data/feedback.csv')
+fs = s3fs.S3FileSystem(anon=False)
+
+@st.experimental_memo(ttl=60)
+def read_file(filename):
+	with fs.open(filename) as f:
+		return f.read().decode("utf-8")
+
+messages_df = read_file('boatpartystreamlit2/feedback.csv')
+# messages_df = read_file('boatpartystreamlit/feedback.csv')
+# messages_df = pd.read_csv('./data/feedback.csv')
 for messages in messages_df.index:
 	name = messages_df.loc[messages, 'Name']
 	content = messages_df.loc[messages, 'Content']
@@ -27,6 +38,6 @@ if st.button('submit'):
 	else:
 		df2 = pd.DataFrame([[name,comment]], columns = ['Name', 'Content'])
 		messages_df = messages_df.append(df2, ignore_index=True)
-		messages_df.to_csv('./data/feedback.csv')
+		# messages_df.to_csv('./data/feedback.csv')
 
 
