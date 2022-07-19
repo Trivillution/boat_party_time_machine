527fdfdfce4e6398fd7a2928d05b7b30ec0d7d28
Yuanjie Chen <yuanjiechen@Yuanjies-MacBook-Air.local>
2022-07-19 02:08:22 +0100

    test gitignore

--- Boat_Party_Time_Machine⏱.py
+++ Boat_Party_Time_Machine⏱.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 
 image = Image.open('./data/logo.PNG')
 st.image(image, width=200)
@@ -8,3 +9,24 @@ st.title('CCS Alumni Boat Party 2022')
 st.subheader('About US')
 
 st.subheader('technical support contact')
+
+st.subheader('留言板')
+messages_df = pd.read_csv('./data/feedback.csv')
+for messages in messages_df.index:
+	name = messages_df.loc[messages, 'Name']
+	content = messages_df.loc[messages, 'Content']
+	st.text(name + ':')
+	st.text(content)
+
+
+comment = st.text_area('有什么话想跟大家说的，请在这里留言哦')
+name = st.text_input('昵称', '')
+if st.button('submit'):
+	if name == '':
+		st.warning('请输入一个昵称')
+	else:
+		df2 = pd.DataFrame([[name,comment]], columns = ['Name', 'Content'])
+		messages_df = messages_df.append(df2, ignore_index=True)
+		messages_df.to_csv('./data/feedback.csv')
+
+
